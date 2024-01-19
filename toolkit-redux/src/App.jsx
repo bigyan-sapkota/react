718f41b0e03aebb82c0feb1451d5591d566fbb9b
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2024-01-19 13:41:48 +0545

    Added Modal , redux-toolkit

--- toolkit-redux/src/App.jsx
+++ toolkit-redux/src/App.jsx
@@ -3,9 +3,11 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 const App = () => {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,6 +15,7 @@ const App = () => {
   }, [cartItems]);
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
