97917ebba1c1828a46159ab86e2858fdfcc29e79
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2024-01-19 12:54:53 +0545

    Added Total Function to cart , redux-toolkit

--- toolkit-redux/src/App.jsx
+++ toolkit-redux/src/App.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 const App = () => {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
